02d75058db7e02512f104ce605eeb6789cd3e93a
Marco Chiapello <chiapello.m@gmail.com>
2022-09-21 16:02:05 +0200

    Presenze e classifica 20220920

--- 001_2223_PGSFOGLIZZESE/U14F/scripts/005_presenze.R
+++ 001_2223_PGSFOGLIZZESE/U14F/scripts/005_presenze.R
@@ -1,3 +1,7 @@
+library(tidyverse)
+library(lubridate)
+library(fs)
+
 source("001_2223_PGSFOGLIZZESE/U14F/scripts/999_functions.R")
 
 pres <- readr::read_tsv("001_2223_PGSFOGLIZZESE/U14F/tmp/presenze.tsv")
@@ -5,17 +9,18 @@ players <- readr::read_table("001_2223_PGSFOGLIZZESE/U14F/tmp/players.tsv")
 
 tail(pres, n = 12)
 
-class <- classifica(players,
-    date = "20220916",
-    assenti = c(11))
+pres <- add(players,
+    date = "20220920",
+    assenti = c(0, 28, 14))
 
 tail(as.data.frame(class), n = 24)
 
-file_copy("001_2223_PGSFOGLIZZESE/U14F/tmp/presenze.tsv",
+fs::file_copy("001_2223_PGSFOGLIZZESE/U14F/tmp/presenze.tsv",
           "001_2223_PGSFOGLIZZESE/U14F/tmp/presenze_old.tsv",
           overwrite = TRUE)
 write_tsv(pres, "001_2223_PGSFOGLIZZESE/U14F/tmp/presenze.tsv")
 
+# Presenze raw
 pres %>% 
     mutate(date = format(date, "%d-%m")) %>% 
     pivot_wider(names_from = date,
@@ -27,6 +32,7 @@ pres %>%
     gtExtras::gt_theme_538()  %>% 
     gt::gtsave("001_2223_PGSFOGLIZZESE/U14F/trainings/presenze.png", expand = 10)
 
+# Presenze Totali
 pres %>% 
     group_by(Numero, Cognome, Nome) %>% 
     summarise(Assenze = sum(assenti)) %>% 
@@ -36,13 +42,15 @@ pres %>%
     gtExtras::gt_theme_538()  %>% 
     gt::gtsave("001_2223_PGSFOGLIZZESE/U14F/trainings/PresenzeTotale.png", expand = 10)
 
+# Presenze mensili
 pres %>% 
     mutate(mese = month(date)) %>% 
     filter(mese == 9) %>% 
-    group_by(Numero, Cognome, Nome, mese) %>% 
+    group_by(Numero, Cognome, Nome) %>% 
     summarise(Assenze = sum(assenti)) %>% 
     ungroup %>% 
     arrange(desc(Assenze), Cognome) %>% 
     gt::gt() %>% 
+    gt::tab_header(title = gt::md("**Settembre**")) %>% 
     gtExtras::gt_theme_538()  %>% 
     gt::gtsave("001_2223_PGSFOGLIZZESE/U14F/trainings/PresenzeSettembre.png", expand = 10)
--- 001_2223_PGSFOGLIZZESE/U14F/scripts/classifica.R
+++ 001_2223_PGSFOGLIZZESE/U14F/scripts/classifica.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(fs)
 
 # Classifica
 source("001_2223_PGSFOGLIZZESE/U14F/scripts/999_functions.R")
@@ -10,8 +11,8 @@ players <- readr::read_table("001_2223_PGSFOGLIZZESE/U14F/tmp/players.tsv")
 tail(class, n = 12)
 
 class <- classifica(players,
-            date = "20220917",
-            vincitori = c(0, 42,54,28,5,14))
+            date = "20220920",
+            vincitori = c(22,11,27))
 
 tail(as.data.frame(class), n = 24)
 
@@ -26,10 +27,11 @@ pres <- readr::read_tsv("001_2223_PGSFOGLIZZESE/U14F/tmp/presenze.tsv")
 
 class %>% 
     left_join(pres) %>% 
+    group_by(date) %>% 
     mutate(Punteggio = case_when(vincitori == 1 & assenti == 0 ~ 3,
                                  vincitori == 0 & assenti == 0 ~ 1,
                                  TRUE ~ 0)) %>% 
-    arrange(desc(Punteggio), Cognome) %>% 
+    arrange(date, desc(Punteggio), Cognome) %>% 
     gt::gt() %>% 
     gtExtras::gt_theme_538()  %>% 
     gt::gtsave("001_2223_PGSFOGLIZZESE/U14F/trainings/ClassificaParziale.png", expand = 10)
